bcb6ea4d5845bd5881c503ca4aadc0060baf5295
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-11 01:24:59 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, contact_details_hash|
+    if name == "Freddy Mercury"
+      contact_details_hash.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if {|flavor| flavor == "strawberry"}
+        end
+      end
+    end
+  end
 end
